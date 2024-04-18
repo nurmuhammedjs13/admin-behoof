fce88d106a928016648b9881e7b58f49ea88faf1
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-04-18 17:27:37 +0600

    asd

--- src/components/authorization/authorization.tsx
+++ src/components/authorization/authorization.tsx
@@ -2,17 +2,6 @@ import { Link } from "react-router-dom";
 import "./authorization.css";
 
 function Authorization() {
-<<<<<<< HEAD
-  return (
-    <>
-      <div className="Authorization">
-        <div className="container">
-          <div className="Authorization_content">
-            <div className="comeIn">
-              <div className="comeIn__cards">
-                <div className="comeIn__cards__card">
-                  <h3 className="comeIn__cards__card-logo">Authorization</h3>
-=======
     return (
         <>
             <div className="Authorization">
@@ -24,24 +13,15 @@ function Authorization() {
                                     <h3 className="comeIn__cards__card-logo">
                                         Authorization
                                     </h3>
->>>>>>> b75828750f7cfec641506e55e5aabf50c0fe042e
 
-                  <div className="comeIn__cards__card__title">
-                    <p className="comeIn__cards__card__title-text">Password</p>
-                    <input className="comeIn__cards__card__title-input"></input>
-                    <hr className="comeIn__cards__card__title-hr" />
-                  </div>
+                                    <div className="comeIn__cards__card__title">
+                                        <p className="comeIn__cards__card__title-text">
+                                            Password
+                                        </p>
+                                        <input className="comeIn__cards__card__title-input"></input>
+                                        <hr className="comeIn__cards__card__title-hr" />
+                                    </div>
 
-<<<<<<< HEAD
-                  <div className="comeIn__cards__card__icons">
-                    <Link
-                      to={"/AddProduct"}
-                      className="comeIn__cards__card__icons-but2"
-                    >
-                      Sign in ➩
-                    </Link>
-                  </div>
-=======
                                     <div className="comeIn__cards__card__icons">
                                         <Link
                                             to={"/AddProduct"}
@@ -52,20 +32,17 @@ function Authorization() {
                                     </div>
                                 </div>
                                 <div className="comeIn__cards_rule">
-                                    <h1 className="comeIn__cards_rule-text">≺ НАПИШИТЕ ПАРОЛЬ</h1>
+                                    <h1 className="comeIn__cards_rule-text">
+                                        ≺ НАПИШИТЕ ПАРОЛЬ
+                                    </h1>
                                 </div>
                             </div>
                         </div>
                     </div>
->>>>>>> b75828750f7cfec641506e55e5aabf50c0fe042e
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Authorization;
